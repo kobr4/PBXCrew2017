b889f770f3dbd27083b44bdfa1cea80b82615e65
AdrienMorvan <adrien.morvan@photobox.com>
2017-02-23 20:41:31 +0100

    Added first bricks of algo logic

--- data_models/cache.py
+++ data_models/cache.py
@@ -3,13 +3,41 @@ import sys
 
 class Cache:
 
-  def _init__(self, capacity=None):
-    self.endpoints = []
+  def _init__(self, index, capacity=None):
+    self.index = index
+    self.endpoints_indexes = []
     self.candidates_videos = {}
     self.selected_videos = []
     self.capacity = capacity
 
   def add_endpoint(self, endpoint_index):
-    self.endpoints.append(endpoint_index)
+    self.endpoints_indexes.append(endpoint_index)
+
+  def add_video_size(self, videos):
+    self.videos = videos
+
+  def create_candidate_videos(self, endpoints):
+
+    for e in self.endpoints_indexes:
+      for video_index in endpoints[e].request.keys():
+        if video_index not in self.candidates_videos:
+          self.candidates_videos[video_index] = 0
+        self.candidates_videos[video_index] += endpoints[e].\
+                                               get_video_scores(self.index, video_index)
+
+    print(self.candidates_videos)
+
+  def pick_candidate(self, endpoints, caches):
+    best_index = sorted([(idx, self.candidates_videos[idx])
+                         for idx in self.candidates_videos.keys()], key=lambda t: -t[1])[0]
+    self.selected_videos.append(best_index)
+    del self.selected_videos[best_index]
+
+    already_notified = []
+    for e in self.endpoints_indexes:
+      already_notified = endpoints[e].notify_caches(caches=caches,
+                                                    cache_index=self.index,
+                                                    video_index=best_index,
+                                                    already_notified=already_notified)
 
 
--- data_models/endpoint.py
+++ data_models/endpoint.py
@@ -3,17 +3,30 @@ import sys
 
 class Endpoint:
 
-  def _init__(self, db_latency=None):
-    self.caches = {}
-    self.requests = {}
-    self.database = db_latency
+  def _init__(self, index, db_latency=None):
+    self.index = index
+    self.caches_lat = {}
+    self.requests_nb = {}
+    self.database_lat = db_latency
 
   def add_cache(self, cache_index, latency):
-    self.caches[cache_index] = latency
+    self.caches_lat[cache_index] = latency
 
   def add_request(self, video_index, nb):
-    self.requests[video_index] = nb
+    self.requests_nb[video_index] = nb
 
   def get_video_scores(self, cache_index, video_index):
-    return self.caches[cache_index] * self.requests[video_index]
+    return (self.db_latency - self.caches_lat[cache_index]) * self.requests_nb[video_index]
+
+  def notify_caches(self, caches, cache_index, video_index, already_notified):
+    score = self.get_video_scores(cache_index, video_index)
+    for c in caches:
+      if c.index in already_notified:
+        continue
+      if video_index in c.candidates_video.keys():
+        c.candidates_video[video_index] -= self.get_video_scores(c.index, video_index)
+        already_notified.append(c.index)
+        #v2 c.candidates_video[video_index] -= score
+    return already_notified
+
 
